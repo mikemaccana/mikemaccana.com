197a07f885acfb18377d8c61d0d1896ce43a3618
Mike MacCana <mike.maccana@gmail.com>
2014-01-22 15:40:43 +0000

    Import blog content

--- importblog.js
+++ importblog.js
@@ -12,14 +12,18 @@ parseString(xml, function (err, result) {
 
   var posts = blog.item
 
+  var count = posts.length
+
+  var results = [];
+
+  console.log(count, 'posts to import')
+
   posts.forEach(function(post){
 
-  var results;
-
     if ( post['wp:post_type'][0] === 'page' ) {
-      // console.log('\n\n')
+      //console.log('\n\n')
 
-      // console.dir(post)
+      //console.dir(post)
       var postName = post.link[0].avstrip('/');
       var html = post['content:encoded'][0]
 
@@ -30,16 +34,41 @@ parseString(xml, function (err, result) {
           function (errors, window) {
             var imageURL = window.$("img").attr('src')
             var extension = imageURL.split('?')[0].split('.').reverse()[0] || 'unknown'
-            console.log(postName, imageURL, extension);
-            request(imageURL).pipe(fs.createWriteStream(postName+'.'+extension))
+            // console.log(postName, imageURL, extension);
+
+            var cleanPost = {
+              slug: postName,
+              previewImage: postName+'.'+extension,
+              title: post.title[0],
+              pubDate: post.pubDate[0],
+              content: post['content:encoded'][0]
+            }
+            //console.log(cleanPost)
+            results.push(cleanPost)
+
+            count--;
+            console.log(count)
+
+            if ( count === 0 ) {
+              console.log('done!')
+
+              fs.writeFile('works.json', JSON.stringify(results, null, 2), function(err) {
+                  if(err) {
+                    console.log(err);
+                  } else {
+                    console.log("JSON saved!");
+                  }
+              });
+            }
+
+
           }
         );
+      } else {
+        count--;
       }
-
-
+    } else {
+      count--;
     }
-
-
-
   })
 });
