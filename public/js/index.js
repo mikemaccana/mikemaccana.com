2b6ef06898c59a2ff95697cfb2693442ed47b1a6
Mike MacCana <mike.maccana@gmail.com>
2014-07-13 22:46:45 +0100

    Fix content when menu is active

--- public/js/index.js
+++ public/js/index.js
@@ -178,31 +178,39 @@ define(function(require){
     var bigM = monogram.select('#big-m');
     var smallM = monogram.select('#small-m');
     var works = queryAll('.works .work')
+    var worksWrapper = query('.works-wrapper')
     var workDescription = query('.work-description')
+    var content = query('.content')
     var title = query('.intro h1');
     var sub = query('.intro h2');
     // See http://raphaeljs.com/reference.html#Element.transform for transform syntax
+    bigM.animate({
+      transform: "T70,0",
+      opacity: 1,
+      fill: 'whiteSmoke'
+    }, 1000, mina.easeout)
+    smallM.animate({
+      transform: "T0,70",
+      opacity: 1,
+      fill: 'white'
+    }, 1500, mina.easeout)
 
-      bigM.animate({
-        transform: "T70,0",
-        opacity: 1,
-        fill: 'whiteSmoke'
-      }, 1000, mina.easeout)
-      smallM.animate({
-        transform: "T0,70",
-        opacity: 1,
-        fill: 'white'
-      }, 1500, mina.easeout)
     setTimeout(function(){
       title.classList.toggle('visible');
     }, 750)
+
     setTimeout(function(){
       sub.classList.toggle('visible');
     }, 1000)
 
     setTimeout(function(){
-      body.classList.remove('loading');
+      content.classList.remove('loading');
     }, 1500)
+
+    setTimeout(function(){
+      worksWrapper.classList.remove('loading');
+      workDescription.classList.remove('loading');
+    }, 2000)
 
     imagesLoaded('.works .work', function(event) {
       log('works loaded');
