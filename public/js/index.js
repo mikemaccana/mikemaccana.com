f2847f7b7b915142f4d4050ac37a493bb8bd6696
Mike MacCana <mike.maccana@gmail.com>
2014-11-11 17:39:09 +0000

    Move monogram into separate module

--- public/js/index.js
+++ public/js/index.js
@@ -10,8 +10,8 @@ define(function(require){
   var unused = require("classlist"),
     agave = require("agave"),
     Ractive = require("ractive"),
+    drawMongogram = require("monogram"),
     imagesLoaded = require("imagesloaded"),
-    Snap = require("snap"),
     worksTemplate = require("text!/views/works.html"),
     workDescriptionTemplate = require("text!/views/workdescription.html"),
     modalTemplate = require("text!/views/modal.html"),
@@ -207,57 +207,7 @@ define(function(require){
     select(selected);
   }
 
-  var drawMongogram = function(){
-    log('Drawing monogram')
-    var monogram = Snap.select(".monogram"),
-      bigM = monogram.select('#big-m'),
-      smallM = monogram.select('#small-m'),
-      works = queryAll('.works .work'),
-      worksWrapper = query('.works-wrapper'),
-      workDescription = query('.work-description'),
-      content = query('.content'),
-      title = query('.intro h1'),
-      sub = query('.intro h2');
 
-    // See http://raphaeljs.com/reference.html#Element.transform for transform syntax
-    bigM.animate({
-      transform: "T70,0",
-      opacity: 1,
-      fill: 'whiteSmoke'
-    }, 1000, mina.easeout)
-    smallM.animate({
-      transform: "T0,70",
-      opacity: 1,
-      fill: 'white'
-    }, 1500, mina.easeout)
-
-    setTimeout(function(){
-      title.classList.toggle('visible');
-    }, 750)
-
-    setTimeout(function(){
-      sub.classList.toggle('visible');
-    }, 1000)
-
-    setTimeout(function(){
-      content.classList.remove('loading');
-    }, 1500)
-
-    setTimeout(function(){
-      imagesLoaded('.works .work', function(event) {
-        worksWrapper.classList.remove('loading');
-        workDescription.classList.remove('loading');
-        // Fade in each item individually
-        works.avforEach(function(element, index){
-          setTimeout(function(){
-            element.classList.toggle('visible');
-          }, index * 300)
-        })
-        workDescription.classList.toggle('visible');
-      })
-    }, 2000)
-
-  }
 
   drawMongogram();
 
