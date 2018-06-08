0706c248e8bfce33cda80e18e036c0f340583a03
Mike MacCana <mike@certsimple.com>
2018-06-08 20:45:44 +0100

    Remore requirejs crap

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,26 @@
-requirejs.config({
-  // By default load any module IDs from js dir
-  baseUrl: 'js'
+var log = console.log.bind(console);
+
+var showPortfolio = require("portfolio"),
+	drawMongogram = require("monogram")
+
+var query = document.querySelector.bind(document),
+	queryAll = document.querySelectorAll.bind(document);
+
+NodeList.prototype.forEach = Array.prototype.forEach;
+
+var menuToggledElements = queryAll('nav, body, .hamburger, .content, footer');
+
+// Clicking ☰ button displays nav
+query('.hamburger').addEventListener('click', function(event) {
+	menuToggledElements.forEach(function(element, index){
+		element.classList.toggle('menu-active');
+	});
+	event.preventDefault();
 });
 
-define(function(require){
-  "use strict";
-  var log = console.log.bind(console);
+// Portfolio page stuff
+if ( window.location.pathname === '/' ) {
+	showPortfolio();
+}
 
-  var unused = require("classlist"),
-    showPortfolio = require("portfolio"),
-    drawMongogram = require("monogram")
-
-  var query = document.querySelector.bind(document),
-    queryAll = document.querySelectorAll.bind(document);
-
-  NodeList.prototype.forEach = Array.prototype.forEach;
-
-  var menuToggledElements = queryAll('nav, body, .hamburger, .content, footer');
-
-  // Clicking ☰ button displays nav
-  query('.hamburger').addEventListener('click', function(event) {
-    menuToggledElements.forEach(function(element, index){
-      element.classList.toggle('menu-active');
-    });
-    event.preventDefault();
-  });
-
-  // Portfolio page stuff
-  if ( window.location.pathname === '/' ) {
-    showPortfolio();
-  }
-
-  drawMongogram();
-
-})
-
+drawMongogram();
