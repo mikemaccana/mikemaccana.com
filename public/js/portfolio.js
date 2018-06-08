d71fa15350b3ca2a89dde3bc743f50d23ce84517
Mike MacCana <mike@certsimple.com>
2018-06-08 22:48:48 +0100

    Add missign method (agave doesn't do DOM stuff anymore)

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -23,6 +23,11 @@ var ITEM_WIDTH = {
 	unselected: 180
 };
 
+// Return index of node under its parents. Eg, if you're the fourth child, return 3.
+Element.prototype.avgetParentIndex = function() {
+	return Array.prototype.indexOf.call(this.parentNode.children, this);
+}
+
 const showPortfolio = function(){
 	var worksRactive = new Ractive({
 		el: '.works-container',
