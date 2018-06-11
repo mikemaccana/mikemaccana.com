e0a7668502bb15cad5fb752d797fc8ce78df921d
Mike MacCana <mike@certsimple.com>
2018-06-11 14:48:39 +0100

    Get rid of ul / li bullshit

--- public/js/templates/works.js
+++ public/js/templates/works.js
@@ -1,13 +1,13 @@
 var worksTemplate = `
 <div class="slider js_slider">
 	<div class="frame js_frame">
-		<ul class="slides js_slides">
+		<div class="slides js_slides">
 			{{#works}}
-				<li class="js_slide" data-logo="/images/logos/{{{ client }}}.png">
+				<div class="js_slide" data-logo="/images/logos/{{{ client }}}.png">
 					<img src="/images/work/thumbs/{{{ previewImage }}}"/>
-				</li>
+				</div>
 			{{/works}}
-		</ul>
+		</div>
 	</div>
 	<span class="js_prev prev">
 		<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 501.5 501.5"><g><path fill="#2E435A" d="M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z"/></g></svg>
