a9560c2dfc3e6804b06e4a938db5c9f08062a4b7
Mike MacCana <mike@certsimple.com>
2018-07-18 11:48:40 +0100

    Remove ES5 transpilation

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import buble from 'rollup-plugin-buble';
 import uglify from 'rollup-plugin-uglify';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -39,7 +38,6 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), transpile and minify
-		production && buble({ include: ['src/**', 'node_modules/svelte/shared.js'] }),
 		production && uglify()
 	]
 };
