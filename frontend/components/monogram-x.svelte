00c32ab331040f33e33709b18d512d1c60a3e8ca
Mike MacCana <mike.maccana@gmail.com>
2020-01-20 19:46:25 +0000

    Remove dead SVG

--- frontend/components/monogram-x.svelte
+++ frontend/components/monogram-x.svelte
@@ -4,8 +4,6 @@
   import { onMount } from "svelte";
   console.log(`Hello from monogram.svelte`);
 
-  // log(`mina is ${mina}`)
-
   onMount(function() {
     log("Drawing monogram");
     var monogram = Snap.select(".monogram"),
@@ -111,42 +109,29 @@
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
-    <defs />
     <g
-      id="Page-1"
-      stroke="none"
-      stroke-width="1"
-      fill="none"
-      fill-rule="evenodd"
-      sketch:type="MSPage">
-      <g id="monogram" sketch:type="MSLayerGroup" fill="#222222">
-        <g
-          id="big-m"
-          transform="translate(67.000000, 209.000000)"
-          sketch:type="MSShapeGroup">
-          <path
-            d="M0,0 L0,544 L12,560 L12,28 L125,131 L81,175 L132.710938,223
-            L132.710938,207 L97,175 L132.710938,140 L132.710938,123 L0,0 Z"
-            id="left" />
-          <path
-            d="M133.644531,0 L133.644531,544 L145.644531,560 L145.644531,28
-            L256.644531,132 L214.644531,175 L266.355469,223 L266.355469,207
-            L230.644531,175 L266.355469,140 L266.355469,123 L133.644531,0 Z"
-            id="right"
-            transform="translate(200.000000, 280.000000) scale(-1, 1)
-            translate(-200.000000, -280.000000) " />
-        </g>
-        <g id="small-m" sketch:type="MSShapeGroup">
-          <path
-            d="M0,1 L0,402 L12,417 L12,29 L201,217 L201,201 L0,1 Z"
-            id="left" />
-          <path
-            d="M200,0 L200,401 L212,416 L212,28 L401,216 L401,200 L200,0 Z"
-            id="right"
-            transform="translate(300.500000, 208.000000) scale(-1, 1)
-            translate(-300.500000, -208.000000) " />
-        </g>
-      </g>
+      id="big-m"
+      transform="translate(67.000000, 209.000000)"
+      sketch:type="MSShapeGroup">
+      <path
+        d="M0,0 L0,544 L12,560 L12,28 L125,131 L81,175 L132.710938,223
+        L132.710938,207 L97,175 L132.710938,140 L132.710938,123 L0,0 Z"
+        id="left" />
+      <path
+        d="M133.644531,0 L133.644531,544 L145.644531,560 L145.644531,28
+        L256.644531,132 L214.644531,175 L266.355469,223 L266.355469,207
+        L230.644531,175 L266.355469,140 L266.355469,123 L133.644531,0 Z"
+        id="right"
+        transform="translate(200.000000, 280.000000) scale(-1, 1)
+        translate(-200.000000, -280.000000) " />
+    </g>
+    <g id="small-m" sketch:type="MSShapeGroup">
+      <path d="M0,1 L0,402 L12,417 L12,29 L201,217 L201,201 L0,1 Z" id="left" />
+      <path
+        d="M200,0 L200,401 L212,416 L212,28 L401,216 L401,200 L200,0 Z"
+        id="right"
+        transform="translate(300.500000, 208.000000) scale(-1, 1)
+        translate(-300.500000, -208.000000) " />
     </g>
   </svg>
   <h1>Mike MacCana</h1>
