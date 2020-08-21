a6c99262e8a5cf123b7a5d043b2baf435b1d53b3
Mike MacCana <mike.maccana@gmail.com>
2020-08-21 14:21:42 +0100

    article working

--- frontend/components/article.svelte
+++ frontend/components/article.svelte
@@ -1,11 +1,19 @@
 <script>
   import { router, Link } from "yrv";
   import Heading from "./heading.svelte";
-  // import axios from "axios";
+  // import { superagent } from "superagent";
   export let slug = $router.params.slug;
+  export let articles;
+  const log = console;
 
-  let title = slug;
-  let subtitles = [];
+  // let title = slug;
+  // let subtitles = [];
+
+  // const response = superagent.get(`localhost/api/articles/${slug}`);
+
+  let article = articles.find(article => article.slug === slug);
+  let title = article.title;
+  let subtitles = [article.description];
 </script>
 
 <style>
